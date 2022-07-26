7495c06f710ed17c8d10594817ca5dc6b2b52e97
ariefhk <ariefrachmanhakim1001@gmail.com>
2022-07-26 16:12:30 +0700

    update answer quiz

--- Javascript Chalangge/2_day.js
+++ Javascript Chalangge/2_day.js
@@ -28,6 +28,33 @@ const numberOfQuiz = (NumberOfQuiz) => {
 // 23.Use match() method to find all the a’s in 30 Days Of JavaScript
 // 24.Use concat() and merge '30 Days of' and 'JavaScript' to a single string, '30 Days Of JavaScript'
 // 25.Use repeat() method to print 30 Days Of JavaScript 2 times
+// 26.Using console.log() print out the following statement:
+// The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.
+// 27.Using console.log() print out the following quote by Mother Teresa:
+// "Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."
+// 28.Check if typeof '10' is exactly equal to 10. If not make it exactly equal.
+// 29.Check if parseFloat('9.8') is equal to 10 if not make it exactly equal with 10.
+// 30.Check if 'on' is found in both python and jargon
+// 31.I hope this course is not full of jargon. Check if jargon is in the sentence.
+// 32.Generate a random number between 0 and 100 inclusively.
+// 33.Generate a random number between 50 and 100 inclusively.
+// 34.Generate a random number between 0 and 255 inclusively.
+// 35.Access the 'JavaScript' string characters using a random number.
+// 36.Use console.log() and escape characters to print the following pattern.
+// 1 1 1 1 1
+// 2 1 2 4 8
+// 3 1 3 9 27
+// 4 1 4 16 64
+// 5 1 5 25 125
+// 37.Use substr to slice out the phrase because because because from the following sentence:'You cannot end a sentence with because because because is a conjunction'
+// 38.'Love is the best thing in this world. Some found their love and some are still looking for their love.' Count the number of word love in this sentence.
+// 39.Use match() to count the number of all because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+// 40.Clean the following text and find the most frequent word (hint, use replace and regular expressions).
+// const sentence =
+//   "%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching";
+// 41.Calculate the total annual income of the person by extracting the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
+
+/* ===  ANSWER === */
 
 numberOfQuiz(1);
 let challenge = "30 Days Of JavaScript";
@@ -93,16 +120,105 @@ numberOfQuiz(20);
 console.log(sentence.trim());
 
 numberOfQuiz(21);
-console.log(sentence.startsWith('You'));
+console.log(sentence.startsWith("You"));
 
 numberOfQuiz(22);
 console.log(sentence.endsWith("conjunction"));
 
 numberOfQuiz(23);
-console.log(sentence.match('a'));
+console.log(sentence.match("a"));
 
 numberOfQuiz(24);
-console.log('30 Days of '.concat('JavaScript'));
+console.log("30 Days of ".concat("JavaScript"));
 
 numberOfQuiz(25);
 console.log(sentence.repeat(2));
+
+numberOfQuiz(26);
+console.log(
+  "The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another."
+);
+
+numberOfQuiz(27);
+console.log(
+  "\"Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead\"."
+);
+
+numberOfQuiz(28);
+let var1 = "10",
+  var2 = 10;
+
+// Checking
+if (typeof var1 === typeof var2) {
+  console.log("Equal");
+} else {
+  console.log("Make them Equal");
+  let newVar2 = var2.toString();
+  console.log(typeof var1, typeof newVar2);
+  if (typeof var1 === typeof newVar2) {
+    console.log("Now Equal");
+  }
+}
+
+numberOfQuiz(29);
+let var3 = parseFloat("9.8"),
+  var4 = 10;
+
+// Checking
+if (typeof var3 == typeof var4) {
+  console.log("Equal");
+}
+
+numberOfQuiz(30);
+let python = "Python",
+  jargon = "Jargon";
+
+if (python.includes("on") && jargon.includes("on")) {
+  console.log("Yes");
+}
+
+numberOfQuiz(31);
+let jargonSentence = "I hope this course is not full of jargon.";
+if (jargonSentence.includes("jargon")) {
+  console.log("Yes, includes jargon ");
+}
+
+numberOfQuiz(32);
+let randomNum = Math.random();
+let randomNumToTen = randomNum * 101;
+console.log(randomNumToTen);
+
+let randomNumToTenFixed = Math.floor(randomNumToTen);
+console.log(randomNumToTenFixed);
+
+numberOfQuiz(33);
+let number = Math.floor(Math.random() * 51 + 50);
+console.log(number);
+
+numberOfQuiz(34);
+let number2 = Math.floor(Math.random() * 256);
+console.log(number2);
+
+numberOfQuiz(35);
+const generateString = (Sentence) => {
+  let answer = "";
+  let sentence = Sentence;
+  let possibleChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  let i = 0;
+  while (i < sentence.length) {
+    let getChar = possibleChar.charAt(
+      Math.floor(Math.random() * possibleChar.length)
+    );
+    if (sentence[i].match(getChar)) {
+      answer += getChar;
+      i += 1;
+    } else {
+      console.log(`${getChar} Mencari ${sentence[i]}`);
+    }
+  }
+  return answer;
+};
+// console.log(generateString("JavaScript"));
+
+numberOfQuiz(36);
+console.log('1 1 1 1 1\n2 1 2 4 8\n3 1 3 9 27\n4 1 4 16 64\n5 1 5 25 125');
