c8698956e451cde0ce7ae23684800216d4525e25
ariefhk <ariefrachmanhakim1001@gmail.com>
2022-08-23 14:55:31 +0700

    rebuild

---
+++ 1_intro.js
@@ -0,0 +1,69 @@
+var sayHi = "Hello World";
+console.log(sayHi);
+
+// Data Type
+let name = "Arief Rachman Hakim";
+let angka = 22;
+let todayIsWeekEnd = false;
+
+console.log(name);
+console.log(angka);
+console.log(todayIsWeekEnd);
+
+// Operator
+// Aritmatika
+let angka1 = 5;
+let angka2 = 10;
+
+const tambahAngka = angka1 + angka2;
+const kurangiAngka = angka1 - angka2;
+const kaliAngka = angka1 * angka2;
+const bagiAngka = angka1 / angka2;
+const modAngka = angka1 % angka2;
+
+// Assignment
+let angka3 = 10;
+
+//Perbandingan
+// Equal (==)
+var angka = 50;
+console.log(angka == 50);
+console.log(angka == 15);
+
+// Not Equal (!=)
+var hobby = "coding";
+console.log(hobby != "game");
+console.log(hobby != "mancing");
+
+// Strict Equal (===)
+var angka = 21;
+console.log(angka == "21");
+console.log(angka === "21");
+console.log(angka === 21);
+
+// Strict not Equal (===)
+var angka = 18;
+console.log(angka != "18");
+console.log(angka !== "18");
+console.log(angka !== 18);
+
+// Kurang dari & Lebih Dari (<, >, <=, >=)
+var angka = 15;
+console.log(angka < 20);
+console.log(angka > 15);
+console.log(angka >= 15);
+console.log(angka <= 20);
+
+// Kondisional
+// OR
+console.log(true || true);
+console.log(true || false);
+console.log(true || false || false);
+console.log(false || false);
+
+// AND
+console.log(true && true);
+console.log(true && false);
+console.log(false && false);
+console.log(false && true && true);
+console.log(true && true && true);
